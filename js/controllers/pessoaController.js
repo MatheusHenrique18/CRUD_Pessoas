e792e6cb566bae3a92c3af5a7758d0f6f19b1b7c
MatheusHenrique18 <matheus.hsb18@gmail.com>
2020-03-30 20:57:33 -0300

    Trabalhando com services, utilizando-se do conceito singleton.

---
+++ js/controllers/pessoaController.js
@@ -0,0 +1,110 @@
+var cadastroPessoa = angular.module("cadastroPessoa", []);
+
+cadastroPessoa.controller("pessoaController", function($scope, $http, pessoasAPI){
+    $scope.app = "Registro de Pessoas";
+    $scope.messageSuccess = "";
+    $scope.pessoas = [];
+    
+    //apenas para indicar id do novo registro a ser criado pelo usuário
+    $scope.sequenceID = 0;
+
+    function listarPessoas(){
+        // $http({
+        //     method: 'GET',
+        //     url: 'http://localhost:3000/pessoas'
+        //  }).then(function (response){
+        //     $scope.pessoas = response.data;
+        //     $scope.sequenceID = $scope.pessoas.length + 1; 
+        //  },function (error){
+        //      error = "Sistema não conseguiu carregar registros do *db.json*. Erro ao carregar response.data de pessoas.";
+        //     console.log(error);
+        //  });
+
+        pessoasAPI.getPessoas().then(function(response){
+            $scope.pessoas = response.data;
+            $scope.sequenceID = $scope.pessoas.length + 1;
+        },function(error){
+            error = "Sistema não conseguiu carregar registros do *db.json*. Erro ao carregar response.data de pessoas.";
+            console.log(error);
+        });
+    }
+    
+
+    $scope.cadastrarPessoa = function(pessoa){
+        // $http({
+        //     method: 'POST',
+        //     url: 'http://localhost:3000/pessoas',
+        //     data: pessoa
+        //  }).then(function (response){
+        //     $scope.messageSuccess = "Registro cadastrado com sucesso!";
+        //     listarPessoas();
+        //  },function (error){
+        //      error = "Sistema não conseguiu cadastrar pessoa corretamente.";
+        //     console.log(error);
+        //  });
+
+        pessoasAPI.savePessoas(pessoa).then(function(response){
+            $scope.messageSuccess = "Registro cadastrado com sucesso!";
+            listarPessoas();
+        },function(error){
+            error = "Sistema não conseguiu cadastrar pessoa corretamente.";
+            console.log(error);
+        });
+    };
+
+    $scope.selecionarPessoa = function(pessoa){
+        $scope.pessoaSelecionada = pessoa;
+        console.log($scope.pessoaSelecionada);
+    };
+
+    $scope.alterarPessoa = function(pessoaSelecionada){
+        // $http({
+        //     method: 'PUT',
+        //     url: 'http://localhost:3000/pessoas/'+pessoaSelecionada.id,
+        //     data: pessoaSelecionada
+        //  }).then(function (response){
+        //     $scope.messageSuccess = "Registro alterado com sucesso!";
+        //     listarPessoas();
+        //  },function (error){
+        //      error = "Sistema não conseguiu identificar o id para alterar a pessoa correta.";
+        //     console.log(error);
+        //  });
+
+        pessoasAPI.editPessoas(pessoaSelecionada).then(function(response){
+            $scope.messageSuccess = "Registro alterado com sucesso!";
+            listarPessoas();
+        },function(error){
+            error = "Sistema não conseguiu identificar o id para alterar a pessoa correta.";
+            console.log(error);
+        });
+
+    };
+
+    $scope.excluirPessoa = function(pessoaSelecionada){
+        // $http({
+        //     method: 'DELETE',
+        //     url: 'http://localhost:3000/pessoas/'+$scope.pessoaSelecionada.id
+        //  }).then(function (response){
+        //     $scope.messageSuccess = "Registro excluído com sucesso!";
+        //     listarPessoas();
+        //  },function (error){
+        //      error = "Sistema não conseguiu identificar o id para excluir a pessoa correta.";
+        //     console.log(error);
+        //  });
+        
+        pessoasAPI.deletePessoas(pessoaSelecionada).then(function(response){
+            $scope.messageSuccess = "Registro excluído com sucesso!";
+            listarPessoas();
+        },function(error){
+            error = "Sistema não conseguiu identificar o id para excluir a pessoa correta.";
+            console.log(error);
+        });
+
+    };
+
+    $scope.clearMessages = function(){
+        $scope.messageSuccess = "";
+    };
+
+    listarPessoas();
+});
---
+++ js/services/pessoasAPIService.js
@@ -0,0 +1,24 @@
+angular.module("cadastroPessoa").factory("pessoasAPI", function($http){
+    var _getPessoas = function(){
+        return $http.get("http://localhost:3000/pessoas");
+    };
+    
+    var _savePessoas = function(pessoa){
+        return $http.post("http://localhost:3000/pessoas", pessoa);
+    };
+
+    var _editPessoas = function(pessoaSelecionada){
+        return $http.put("http://localhost:3000/pessoas/"+ pessoaSelecionada.id, pessoaSelecionada);
+    };
+
+    var _deletePessoas = function(pessoaSelecionada){
+        return $http.delete("http://localhost:3000/pessoas/"+ pessoaSelecionada.id);
+    };
+
+    return{
+        getPessoas: _getPessoas,
+        savePessoas: _savePessoas,
+        editPessoas: _editPessoas,
+        deletePessoas: _deletePessoas
+    };
+});
